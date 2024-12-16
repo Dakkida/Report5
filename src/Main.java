fdb64e1a7494ce0f195ed6f7be44e10de3813fbc
Daiki Uehara <e245736@ie.u-ryukyu.ac.jp>
2024-12-17 01:59:08 +0900

    step3APIリファレンスを参照してみよう

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("中身がNULLです!!");
-            System.out.println("ERROR!:" + e.getMessage());
-        }
-        
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }
 }
